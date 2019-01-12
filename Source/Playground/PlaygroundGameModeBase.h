c7639d44ccd1a6fe35ef6f77fedb8247fa332af9
Chris McCabe <christopherdentist@gmail.com>
2019-01-12 09:48:11 -0800

    Initial commit

---
+++ Source/Playground/PlaygroundGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlaygroundGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAYGROUND_API APlaygroundGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
